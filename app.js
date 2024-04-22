9bbd5c3fd36f58aa91a57d279fbd6d9dc8b32b82
Stephan Mutarushwa <stephanemugisho24@gmail.com>
2024-04-22 04:54:21 +0200

    creation d'autres fichier

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-let a= 22
-console.log("Hello " + a);
+const express = require('express')
 
+const app= express()
+const port = 3000
+app.get('/', (req,res)=>res.send("hello again, Express 2"))
+
+app.get('/api/pokemons/:id',(req, res)=>{
+    const id = req.params.id //recuperer l'id contenu dans l'url
+    res.send(`Vous avez demande le pokemon numero ${id}`)})
+
+
+
+app.listen(port,()=>console.log(`Notre application est demarree sur : http://localhost:${port}`))
