6a7446dd5d26e4091276b50207dac6cf2a8d1f73
Stephan Mutarushwa <stephanemugisho24@gmail.com>
2024-05-03 09:39:59 +0300

    ajout du fichier mock-pokemo.js et quelques modifs dans app.js

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express')
+let pokemons = require('./mock-pokemon');
 
 const app= express()
 const port = 3000
+
 app.get('/', (req,res)=>res.send("hello again, Express 2"))
 
 app.get('/api/pokemons/:id',(req, res)=>{
-    const id = req.params.id //recuperer l'id contenu dans l'url
-    res.send(`Vous avez demande le pokemon numero ${id}`)})
+    const id = parseInt(req.params.id) //recuperer l'id contenu dans l'url
+    const pokemon= pokemons.find(pokemon => pokemon.id ===id)
+    res.send(`Vous avez demande le pokemon numero ${pokemon.name}.`)
+})
+
+app.get('/api/pokemons',(req, res)=>{
+    res.send(`Il y a ${pokemons.length} pokemons dans le pokedex pour le moment.`)
+})
+
 
 
 
