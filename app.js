2e142b2dbb48229d0fba6251a6416ac83573542b
Stephan Mutarushwa <stephanemugisho24@gmail.com>
2024-05-03 10:40:00 +0300

    ajout de morgan

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express')
+const {success} = require('./helper.js')
 let pokemons = require('./mock-pokemon');
 
 const app= express()
 const port = 3000
 
+
+app.use((req, res, next)=>{
+    console.log(`URL : ${req.url}`)
+    next()
+})
+
 app.get('/', (req,res)=>res.send("hello again, Express 2"))
+
+app.get('/api/pokemons/:id', (req, res)=>{
+    const message= "la liste des pokemons a bien ete recuperees"
+    res.json(success(message,pokemons))
+})
 
 app.get('/api/pokemons/:id',(req, res)=>{
     const id = parseInt(req.params.id) //recuperer l'id contenu dans l'url
     const pokemon= pokemons.find(pokemon => pokemon.id ===id)
-    res.send(`Vous avez demande le pokemon numero ${pokemon.name}.`)
+    const message = "Un pokemon a bien ete trouve."
+    res.json(success(message, pokemon))
 })
 
 app.get('/api/pokemons',(req, res)=>{
