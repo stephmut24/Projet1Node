590c6f4e90f10578a6df5e16d58b73dd04bce7c6
Stephan Mutarushwa <stephanemugisho24@gmail.com>
2024-05-05 18:51:16 +0300

    modif

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const favicon = require('serve-favicon')
+const bodyParser = require('body-parser')
 const {success, getUniqueId} = require('./helper.js')
 let pokemons = require('./mock-pokemon');
 
@@ -11,10 +12,11 @@ const port = 3000
 app
 .use(favicon(__dirname + '/favicon.ico'))
 .use(morgan('dev'))
+.use(bodyParser.json())
 
 app.get('/', (req,res)=>res.send("hello again, Express 2"))
 
-app.get('/api/pokemons/:id', (req, res)=>{
+app.get('/api/pokemons/', (req, res)=>{
     const message= "la liste des pokemons a bien ete recuperees"
     res.json(success(message,pokemons))
 })
@@ -37,7 +39,23 @@ app.post('/api/pokemons', (req, res)=>{
     const message = `Le pokemon ${pokemonCreated.name} a bien ete cree.`
     res.json(success(message, pokemonCreated))
 })
+app.put('/api/pokemons/:id', (req, res)=>{
+    const id = parseInt(req.params.id)
+    const pokemonUpdated = { ...req.body, id: id}
+    pokemons = pokemons.map(pokemon =>{
+        return  pokemon.id === id ? pokemonUpdated : pokemon
+    })
+    const message = `Le pokemon ${pokemonUpdated.name} a bien ete modifie.`
+    res.json(success(message, pokemonUpdated))
 
+})
+app.delete('/api/pokemons/:id', (req, res)=>{
+    const id = parseInt(req.params.id)
+    const pokemonDeleted = pokemons.find(pokemon =>pokemon.id ===id)
+    pokemons.filter(pokemon => pokemon.id !== id)
+    const message = `Le pokemon ${pokemonDeleted.name} a bien ete supprime.`
+    res.json(success(message, pokemonDeleted))
+})
 
 
 
