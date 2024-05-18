06aced7a363fe609dd6b1c976ebef8a0fe404e4a
Stephan Mutarushwa <stephanemugisho24@gmail.com>
2024-05-18 05:15:48 +0300

    login

--- src/roots/login.js
+++ src/roots/login.js
@@ -1,5 +1,7 @@
 const { User } = require('../db/sequelize')
 const bcrypt = require('bcrypt')
+const jwt =require('jsonwebtoken')
+const privateKey = require('../auth/private_key')
 
 module.exports = (app) =>{
     app.post('/api/login', (req, res)=>{
@@ -10,13 +12,22 @@ module.exports = (app) =>{
                 const message = `L'utilisateur demande n'existe pas.`
                 return res.status(404).json({message})
             }
+
             bcrypt.compare(req.body.password, user.password).then(isPasswordValid => {
                 if(isPasswordValid) {
                     const message =`L'utilisateur a ete connecte avec succes`;
                     return res.json({message, data: user})
                 }
+
+                //jwt
+                const token = jwt.sign(
+                    {userId: user.id},
+                    privateKey,
+                    {expireIn: '24h'}
+                )
+
                 const message = `L'utilisateur a ete connecte avec succes`;
-                return res.json({message, data: user})
+                return res.json({message, data: user, token})
             })
         })
         .catch(error =>{
