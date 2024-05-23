285978b52ac564738baf5cf1fd85ab9b7df1d81d
Stephan Mutarushwa <stephanemugisho24@gmail.com>
2024-05-23 16:43:06 +0300

    finition

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const express = require('express')
-const morgan = require('morgan')
 const favicon = require('serve-favicon')
 const bodyParser = require('body-parser')
 const sequelize = require('./src/db/sequelize')
 
 
 const app= express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 
 
 
 app
 .use(favicon(__dirname + '/favicon.ico')) 
-.use(morgan('dev'))
 .use(bodyParser.json())
 
 sequelize.initDb()
+
+app.get('/', (req, res)=>{
+    res.json("Hello, Heroku")
+})
 
 //ici nous placerons futur points de terminaison
 
